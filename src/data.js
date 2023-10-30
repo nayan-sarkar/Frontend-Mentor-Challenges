1a0cbf454bf43d0805064fe53ca58cd021e6c193
Nayan Sarkar <10292012+nayan-sarkar@users.noreply.github.com>
2023-10-30 09:01:07 -0700

    updates

--- src/data.js
+++ src/data.js
@@ -1,9 +1,15 @@
 const projects = [
     {
+        "projectImage" : "projects/ip-address-tracker.jpg",
+        "projectTitle" : "IP Address Tracker",
+        "githubUrl"  : "https://github.com/nayan-sarkar/IP-Address-Tracker",
+        "liveUrl" : "https://fmc-ip-address-tracker.netlify.app/"
+    },
+    {
         "projectImage" : "projects/08-insure-landing-page-master/design/desktop-preview.jpg",
         "projectTitle" : "Insure Landing Page",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/08-insure-landing-page-master",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/08-insure-landing-page-master/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/08-insure-landing-page-master/"
     },
 
 
@@ -11,13 +17,13 @@ const projects = [
         "projectImage" : "projects/10-news-homepage-main/design/desktop-preview.jpg",
         "projectTitle" : "News Landing Page",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/10-news-homepage-main",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/10-news-homepage-main/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/10-news-homepage-main/"
     },
     {
         "projectImage" : "projects/06-spencer-realty-website-master/design.png",
         "projectTitle" : "Spencer Realty Website",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/06-spencer-realty-website-master",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/06-spencer-realty-website-master"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/06-spencer-realty-website-master"
     },
 
 
@@ -25,7 +31,7 @@ const projects = [
         "projectImage" : "projects/09-four-card-feature-section-master/design/desktop-preview.jpg",
         "projectTitle" : "Four Card Feature Section",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/09-four-card-feature-section-master",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/09-four-card-feature-section-master/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/09-four-card-feature-section-master/"
     },
 
 
@@ -33,7 +39,7 @@ const projects = [
         "projectImage" : "projects/04-testimonials-grid-project/design/desktop-preview.jpg",
         "projectTitle" : "Testimonials Grid Page",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/04-testimonials-grid-project",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/04-testimonials-grid-project/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/04-testimonials-grid-project/"
     },
 
 
@@ -41,14 +47,14 @@ const projects = [
     "projectImage" : "projects/11-intro-component-with-signup-form-master/design/desktop-preview.jpg",
     "projectTitle" : "Intro Component with Sign Up Form",
     "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/11-intro-component-with-signup-form-master",
-    "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/11-intro-component-with-signup-form-master/"
+    "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/11-intro-component-with-signup-form-master/"
     },
 
     {
         "projectImage" : "projects/07-single-price-grid-component-master/design/desktop-preview.jpg",
         "projectTitle" : "Single Price Grid Component",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/07-single-price-grid-component-master",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/07-single-price-grid-component-master/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/07-single-price-grid-component-master/"
     },
 
 
@@ -56,7 +62,7 @@ const projects = [
         "projectImage" : "projects/05-huddle-landing-page-with-single-introductory-section-master/design/desktop-preview.jpg",
         "projectTitle" : "Huddle Landing Page",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/05-huddle-landing-page-with-single-introductory-section-master",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/05-huddle-landing-page-with-single-introductory-section-master/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/05-huddle-landing-page-with-single-introductory-section-master/"
     },
 
 
@@ -64,14 +70,14 @@ const projects = [
         "projectImage" : "projects/03-qr-code-component-main/design/desktop-preview.jpg",
         "projectTitle" : "QR Code Component",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/03-qr-code-component-main/",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/03-qr-code-component-main/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/03-qr-code-component-main/"
     },
 
     {
         "projectImage" : "projects/02-product-preview-card-component-main/design/desktop-preview.jpg",
         "projectTitle" : "Product Preview Card",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/02-product-preview-card-component-main",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/02-product-preview-card-component-main/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/02-product-preview-card-component-main/"
     },
 
 
@@ -79,14 +85,14 @@ const projects = [
         "projectImage" : "projects/01-order-summary-component-main/design/desktop-preview.jpg",
         "projectTitle" : "Order Summary Component",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/01-order-summary-component-main",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/01-order-summary-component-main/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/01-order-summary-component-main/"
     },
 
     {
         "projectImage" : "projects/12-FlexBox-Project/design.png",
         "projectTitle" : "Pure FlexBox Website",
         "githubUrl"  : "https://github.com/nayan-sarkar/UI-UX-Design-to-Code/tree/main/public/projects/12-FlexBox-Project",
-        "liveUrl" : "https://nynsrkr-frontend-mentor-challenges.netlify.app/Projects/12-FlexBox-Project/"
+        "liveUrl" : "https://ui-to-code-projects.netlify.app/Projects/12-FlexBox-Project/"
     },
 
      {
